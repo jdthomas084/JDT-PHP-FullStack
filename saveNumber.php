35d6c163281827f6bd9a5c72c5581cfc1285b4c4
Joshua Thomas <jdthomas084@gmail.com>
2026-01-08 15:58:44 -0500

    Add files via upload

---
+++ saveNumber.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "_input_numbers_from_webpage_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Gather input data from a Form and store it to a variable
+$nPut_number = $_POST['number'];
+
+// Define a SQL QUERY and STORE it to a PHP variable
+$query = "INSERT INTO _InputTable (input_value_columnar_data) VALUES (".$nPut_number.");";
+
+// Database Name: "_input_numbers_from_webpage_db"
+// Table Name: "_InputTable"
+// Column Name: "input_value_columnar_data"
+// Field Name: "$nPut_number"
+
+// Carry out the action
+$conn->query($query);
+
+// Close the connection
+$conn->close();
+?>
+// Display some HTML
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+	<!-- Hyperlink to a Landing Page after PHP script runs. -->
+	<h1><a href="landingPage.html">CLICK HERE</a></h1>
+</body>
+</html>
